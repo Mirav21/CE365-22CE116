f616f65a0e9acb0be6dd11493a33a71f766b15a7
HRMPortal05 <miravsavaliya2004@gmail.com>
2025-02-13 09:28:28 +0530

    Prac-2 added comments, Prac-3 pending

--- Prac2.cpp
+++ Prac2.cpp
@@ -2,63 +2,81 @@
 
 using namespace std;
 
-int main(){
-    char Exit= 'y';
+int main()
+{
+    char Exit = 'y';
 
-    while(Exit=='y'){
-        int no_of_symbols,no_of_states,initial_state,no_of_accepting_states;
+    // Loop to allow multiple FA validations
+    while (Exit == 'y')
+    {
+        int no_of_symbols, no_of_states, initial_state, no_of_accepting_states;
 
-        cout<<"Number of input symbols : ";
-        cin>>no_of_symbols;
-        cout<<endl;
+        // Get the number of input symbols
+        cout << "Number of input symbols : ";
+        cin >> no_of_symbols;
+        cout << endl;
 
-        char inp_symbols[no_of_symbols],curr_symbol;
+        char inp_symbols[no_of_symbols];
 
-        for(int i=0;i<no_of_symbols;i++){
-            cout<<"Input symbol ("<<i+1<<") : ";
-            cin>>inp_symbols[i];
-            cout<<endl;
+        // Input symbols for the FA
+        for (int i = 0; i < no_of_symbols; i++)
+        {
+            cout << "Input symbol (" << i + 1 << ") : ";
+            cin >> inp_symbols[i];
+            cout << endl;
         }
 
-        cout<<"Enter number of states : ";
-        cin>>no_of_states;
-        cout<<endl;
+        // Get the number of states in the FA
+        cout << "Enter number of states : ";
+        cin >> no_of_states;
+        cout << endl;
 
-        cout<<"Initial state : ";
-        cin>>initial_state;
-        cout<<endl;
+        // Input the initial state
+        cout << "Initial state : ";
+        cin >> initial_state;
+        cout << endl;
 
-        cout<<"Number of accepting states : ";
-        cin>>no_of_accepting_states;
-        cout<<endl;
+        // Input the number of accepting states
+        cout << "Number of accepting states : ";
+        cin >> no_of_accepting_states;
+        cout << endl;
 
-        if(no_of_accepting_states>no_of_states){
-            cout<<"Number of states should be less then number of states..."<<endl;
+        // Check if accepting states are valid
+        if (no_of_accepting_states > no_of_states)
+        {
+            cout << "Number of accepting states should be less than number of states..." << endl;
             return 0;
         }
 
         int accepting_states[no_of_accepting_states];
 
-        for(int i=0;i<no_of_accepting_states;i++){
-            cout<<"Accepting state ("<<i+1<<") : ";
-            cin>>accepting_states[i];
-            cout<<endl;
+        // Input accepting states
+        for (int i = 0; i < no_of_accepting_states; i++)
+        {
+            cout << "Accepting state (" << i + 1 << ") : ";
+            cin >> accepting_states[i];
+            cout << endl;
         }
 
-        cout<<endl;
+        cout << endl;
 
         int transition_table[no_of_states][no_of_symbols];
 
-        for(int i=0;i<no_of_states;i++){
-            for(int j=0;j<no_of_symbols;j++){
-                cout<<i+1<<" to "<<inp_symbols[j]<<" -> : ";
-                cin>>transition_table[i][j];
+        // Fill transition table for each state and input symbol
+        for (int i = 0; i < no_of_states; i++)
+        {
+            for (int j = 0; j < no_of_symbols; j++)
+            {
+                cout << i + 1 << " to " << inp_symbols[j] << " -> : ";
+                cin >> transition_table[i][j];
             }
         }
-        cout<<endl;
+        cout << endl;
 
-        char exit='1';
-        while(exit == '1'){
+        char exit = '1';
+        // Loop to allow multiple string validations
+        while (exit == '1')
+        {
             string inp_string;
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             cout << "Input string: ";
@@ -68,35 +86,48 @@ int main(){
             size_t inp_str_length = inp_string.length();
             int curr_state = initial_state;
 
-            for(size_t i=0;i<inp_str_length;i++){
-                for(int j=0;j<no_of_symbols;j++){
-                    if(inp_symbols[j]==inp_string[i]){
-                        curr_state = transition_table[curr_state-1][j];
+            // Process the input string
+            for (size_t i = 0; i < inp_str_length; i++)
+            {
+                for (int j = 0; j < no_of_symbols; j++)
+                {
+                    if (inp_symbols[j] == inp_string[i])
+                    {
+                        curr_state = transition_table[curr_state - 1][j];
                     }
                 }
             }
 
-
-            for(int i=0;i<no_of_accepting_states;i++){
-                if(accepting_states[i]==curr_state){
-                    cout<<"Valid String";
-                    break;
-                }
-                else{
-                    cout<<"Invalid String";
+            // Check if the string ends in an accepting state
+            bool is_valid = false;
+            for (int i = 0; i < no_of_accepting_states; i++)
+            {
+                if (accepting_states[i] == curr_state)
+                {
+                    is_valid = true;
                     break;
                 }
             }
 
-            cout<<"\nFor continuing the validation of string enter (1) or enter (0) for exit... : ";
-            cin>>exit;
-            cout<<endl;
+            if (is_valid)
+            {
+                cout << "Valid String";
+            }
+            else
+            {
+                cout << "Invalid String";
+            }
 
+            // Ask user if they want to validate another string
+            cout << "\nFor continuing the validation of string enter (1) or enter (0) for exit... : ";
+            cin >> exit;
+            cout << endl;
         }
 
-        cout<<"\nFor continuing the validation of another FA enter (y) or enter (n) for exit... : ";
-        cin>>Exit;
-        cout<<endl;
+        // Ask user if they want to define another FA
+        cout << "\nFor continuing the validation of another FA enter (y) or enter (n) for exit... : ";
+        cin >> Exit;
+        cout << endl;
     }
 
     return 0;
